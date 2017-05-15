9f20caec4fbf2809237ccd6662210d61a77c187d
alikayhan <ali.kayhan13@gmail.com>
2017-05-16 00:26:04 +0300

    Add layout constraints for imageView and activityIndicator

--- Virtual Tourist/PhotoCollectionViewCell.swift
+++ Virtual Tourist/PhotoCollectionViewCell.swift
@@ -15,17 +15,31 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        imageView.frame = contentView.frame
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
         activityIndicator.activityIndicatorViewStyle = .whiteLarge
-        activityIndicator.center = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
         activityIndicator.hidesWhenStopped = true
         
         contentView.addSubview(imageView)
         contentView.addSubview(activityIndicator)
+        
+        // Constraints for center, width and height of imageView
+        let horizontalConstraint = NSLayoutConstraint(item: imageView, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1, constant: 0)
+        let verticalConstraint = NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1, constant: 0)
+        let widthConstraint = NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: contentView, attribute: .width, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: contentView, attribute: .height, multiplier: 1, constant: 0)
+        
+        // Constraints for center of activityIndicator
+        let activityIndicatorCenterXConstraint = NSLayoutConstraint(item: activityIndicator, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1, constant: 0)
+        let activityIndicatorCenterYConstraint = NSLayoutConstraint(item: activityIndicator, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1, constant: 0)
+        
+        // Add constraints to resize and reposition imageView and activityIndicator
+        // when device rotates
+        contentView.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint,heightConstraint, activityIndicatorCenterXConstraint, activityIndicatorCenterYConstraint])
     }
     
     required init?(coder aDecoder: NSCoder) {
